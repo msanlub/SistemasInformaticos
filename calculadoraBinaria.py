724e05740aee3a3cec55e499eeb947a4414c4403
msanlub898 <msanlub898@g.educaand.es>
2023-11-04 16:25:42 +0100

    avances

--- calculadoraBinaria.py
+++ calculadoraBinaria.py
@@ -1,43 +1,49 @@
-def id(x):
-    return x
+'''def id(x):
+    return x'''
 
 def leerEntradaUno() ->list:
     '''función que define la petición de entrada al usuario'''
-    return list(map(id,input("Dame un byte: ")))
+    return list(input("Dame un byte: "))
 def leerEntradaDos() ->list:
     '''función que define la petición de entrada al usuario'''
-    return list(map(id,input("Dame otro byte: ")))
+    return list(input("Dame otro byte: "))
 
-def suma(byteUno,byteDos,ac,r):
+def suma(byteUno,byteDos,acarreo):
     '''función que suma cada bit en cada posición dentro del byte'''
-    for n in range[len(byteDos)-1,-1,-1]:
+    resultado = [] #lista de resultado
+    for n in range(len(byteDos)-1,-1,-1):
         #posición bit 8
-        if byteUno[n]==0 and byteDos[n]==1 and ac[-1]==0:
-            return r[n]==1
-        elif byteUno[-1]==1 and byteDos[-1]==0 and ac[-1]==0:
-            return r[-1]==1
-        elif byteUno[-1]==1 and byteDos[-1]==1 and ac[-1]==0:
-            return r[-1]==0 and ac[-2]==1
-        elif byteUno[-1]==0 and byteDos[-1]==0 and ac[-1]==0:
-            return r[-1]==0
-        elif byteUno[-1]==1 and byteDos[-1]==1 and ac[-1]==1:
-            return r[-1]==1 and ac[-2]==1
-        elif byteUno[-1]==0 and byteDos[-1]==1 and ac[-1]==1:
-            return r[-1]==0 and ac[-2]==1
-        elif byteUno[-1]==1 and byteDos[-1]==0 and ac[-1]==1:
-            return r[-1]==0 and ac[-2]==1
-    
-        
+        if byteUno[n]==0 and byteDos[n]==1 and acarreo[n]==0:
+            resultado.append(1)
+        elif byteUno[n]==1 and byteDos[n]==0 and acarreo[n]==0:
+            resultado.append(1)
+        elif byteUno[n]==1 and byteDos[n]==1 and acarreo[n]==0:
+            resultado.append(0) and acarreo.append(1)
+        elif byteUno[n]==0 and byteDos[n]==0 and acarreo[n]==0:
+            resultado.append(0)
+        elif byteUno[n]==1 and byteDos[n]==1 and acarreo[n]==1:
+            resultado.append(1) and acarreo.append(1)
+        elif byteUno[n]==0 and byteDos[n]==1 and acarreo[n]==1:
+            resultado.append(0) and acarreo.append(1)
+        elif byteUno[n]==1 and byteDos[n]==0 and acarreo[n]==1:
+            resultado.append(0) and acarreo.append(1)
+        else:
+            return "Cago en to"
+    return resultado.reverse()
+
 if __name__=="__main__":
 
     #entrada
     byteUno = leerEntradaUno()#lista byte primera
-    byteDos = leerEntradaDos() #lista byte segunda
+    byteDos = leerEntradaDos() #lista byte segundas
+    for caracter in range(len(byteUno)):
+        byteUno[caracter] = int(byteUno[caracter])
+    for caracter in range(len(byteDos)):
+        byteDos[caracter] = int(byteDos[caracter])
+    acarreo = [0,0,0,0,0,0,0,0,0] #lista de acarreo
 
     #proceso
-    ac = [0,0,0,0,0,0,0,0,0] #lista de acarreo
-    r = [0,0,0,0,0,0,0,0,0] #lista de resultado
-    sumaBinario = suma(byteUno,byteDos,ac,r)
+    sumaBinario = suma(byteUno,byteDos,acarreo)
 
     #salida
     print(sumaBinario)
