d805beb2d9143f78a3440c4e3ed2171ca96baf21
msanlub898 <msanlub898@g.educaand.es>
2023-11-10 12:31:12 +0100

    con 3 en raya

--- cifradoRot13.py
+++ cifradoRot13.py
@@ -3,11 +3,11 @@ ABC='abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
 CBA='nñopqrstuvwxyzabcdefghijklmNÑOPQRSTUVWXYZABCDEFGHIJKLM'
 
 def rot13(cifrar:str)->str:
-    mapeo = dict(zip(ABC + CBA, CBA + ABC)) #dict: diccionario // zip: para iterar dos listas en paralelo 
+    mapeo = dict(zip(ABC , CBA)) #dict: diccionario // zip: para iterar dos listas en paralelo 
     return ''.join(mapeo.get(x, x) for x in cifrar) #join: añade simbolos a la lista,para iterar los valores // get: nos devuelve un valor si existe,sino devuelve none
 
 def descifrarRot13(descifrar:str)->str:
-    mapeo = dict(zip(CBA + ABC, ABC + CBA))
+    mapeo = dict(zip(CBA , ABC))
     return ''.join(mapeo.get(x, x) for x in descifrar)
 
 if __name__=="__main__":
